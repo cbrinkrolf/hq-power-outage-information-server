9207181d0f50142b53213114c5e70a27ab7fafac
cbrinkrolf <christoph.brinkrolf@uni-bielefeld.de>
2026-04-17 16:56:08 -0400

    initial spring boot application setup

---
+++ src/main/java/main/SpringApplicationStarter.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringApplicationStarter {
+
+	public static void main(String[] args) {
+		System.out.println("start application ...");
+		ConfigurableApplicationContext context = SpringApplication.run(SpringApplicationStarter.class, args);
+
+		context.close();
+		System.out.println("... application ended");
+
+	}
+
+}
